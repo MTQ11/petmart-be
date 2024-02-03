2b69f892fcfbf45aacfdf107db541819b7698ca3
MTQ <mtquyen1002@gmail.com>
2024-02-03 16:50:32 +0700

    connect DB, setup MVC

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const routes = require("./routes")
+const bodyParser = require("body-parser")
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.json())
+
+routes(app)
+
+
+app.get('/',(req,res)=>{
+    return res.send("HELLO WORD !!!!!")
+})
+
+mongoose.connect(`mongodb://localhost:27017/Petmart`, {serverSelectionTimeoutMS: 3000})
+.then(()=>{
+    console.log("connect mongodb success")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.listen(port,()=>{
+    console.log("Sever is running port: ", port)
+})
