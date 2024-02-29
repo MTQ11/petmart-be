233e8c87a15dec01af59561673967f20e16d0ad0
MTQ <mtquyen1002@gmail.com>
2024-02-29 16:15:56 +0700

    api CRUD type-product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const  userRouter = require('./UserRouter')
 const  productRouter = require('./ProductRouter')
+const TypeProduct = require('./TypeProductRouter')
 
 const routes = (app) =>{
     app.use('/user',userRouter)
     app.use('/product',productRouter)
+    app.use('/type-product',TypeProduct)
 }
 
 module.exports = routes
