094d7ca91f3f714bfde8fac3ea6d1a884a5c0514
tuan3132001 <leanhtuan3103@gmail.com>
2024-03-29 15:31:41 +0700

    fix getAll promotion

--- src/routes/PromotionRouter.js
+++ src/routes/PromotionRouter.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const promotionController = require('../controllers/PromotionController')
 const { authAdminMiddleWare, authUserMiddleWare, authMemberMiddleWare } = require("../middleware/authMiddleware")
 
-router.get('/get-promotion',authMemberMiddleWare, promotionController.getAll)
+router.get('/get-promotion', promotionController.getAll)
 router.get('/create-promotion',authMemberMiddleWare, promotionController.createPromotion)
 router.put('/update-promotion/:id',authMemberMiddleWare, promotionController.updatePromotion)
 router.delete('/delete/:id',authMemberMiddleWare, promotionController.deletePromotion)
